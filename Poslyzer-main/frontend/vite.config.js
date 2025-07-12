c177cf84013b20952a966761ab391d8fea50e14f
wahidullah <wahidmaqs49@gmail.com>
2025-07-12 17:01:26 +0530

    Refactor Dockerfiles and update docker-compose configuration for improved service management and dependency handling

--- Poslyzer-main/frontend/vite.config.js
+++ Poslyzer-main/frontend/vite.config.js
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173
+    }
+  }
 })
-
-
